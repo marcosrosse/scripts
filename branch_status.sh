234227fec1e8cd8aa1e401a77129a39e5e5cac85
Marcos Rosse <marcosrossem@gmail.com>
2019-11-11 18:07:02 -0300

    Show the status of a branch

---
+++ branch_status.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+	return 0;
+elif [ condition ]; then
+	return 0;
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+elif [ $REMOTE = $BASE ]; then
+    echo "Need to push"
+else
+    echo "Diverged"
+fi
