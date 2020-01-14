7b4eaa36f891d60a0722755dcd10ba89cb802d9d
Marcos Rosse <marcosrossem@gmail.com>
2020-01-14 18:33:55 -0300

    Correção de checagem de serviço e gerenciamento de arquivos

--- automate.py
+++ automate.py
@@ -8,19 +8,36 @@ subprocess.run(["apt-get", "install", "nginx", "-y"])
 subprocess.run(["curl", "-fsSL", "https://get.docker.com", "-o", "get-docker.sh"])
 subprocess.run(["sh", "get-docker.sh"])
 
-## Checando se os serviços estão com status active
+# ## Checando se os serviços estão com status active
 
 def servicesToCheck():
-    servicesToCheck = ['docker', 'nginx']
+    services = ['docker', 'nginx']
 
-    for servicesToCheck in servicesToCheck:
-        process =  subprocess.Popen(["systemctl", "is-active",  servicesToCheck], stdout=subprocess.PIPE)
+    for service in services:
+        process =  subprocess.Popen(["systemctl", "is-active",  service], stdout=subprocess.PIPE)
         (output, err) = process.communicate()
         output = output.decode('utf-8')
         
 
         if 'inactive' in output:
-            print("Check os serviços", output) ## Aqui quero apresentar o nome do serviço que está out
+            print("Os seguintes serviços estão desativos:", service)
+            # try:
+            #     trying = subprocess.Popen(["systemctl", "start", service], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            # except:
+            #     print("O serviço:", trying, "falhou a inciar")
         else:
-            print("Serviços ativos....")
+            print("Serviço:", service,"ativo")
 
+servicesToCheck()
+
+## Criando e exportando arquivos de virtualhosts do nginx
+
+def touchVirtualHosts():
+    files = ['app1.conf', 'app2.conf', 'app3.conf']
+    locationConfdNginx = "/etc/nginx/conf.d/"
+    
+    subprocess.run(["rm", "-f", locationConfdNginx,"default.conf"])
+    for touching in files:
+            from pathlib import Path
+            Path(locationConfdNginx,touching).touch()
+            subprocess.run(["ls", "-la", locationConfdNginx])
