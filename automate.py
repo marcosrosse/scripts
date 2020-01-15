ed373f6b63c2235cc19dc38d6eb800e95bf477b4
Marcos Rosse <marcosrossem@gmail.com>
2020-01-15 15:21:42 -0300

    Criação dos arquivos de configuração do nginx

--- automate.py
+++ automate.py
@@ -1,43 +1,46 @@
 import subprocess
 
-## Preparando e instalando pacotes necessarios
+# ## Preparando e instalando pacotes necessarios
 
-subprocess.run(["apt-get", "update", "-y"])
-subprocess.run(["apt-get", "upgrade", "-y"])
-subprocess.run(["apt-get", "install", "nginx", "-y"])
-subprocess.run(["curl", "-fsSL", "https://get.docker.com", "-o", "get-docker.sh"])
-subprocess.run(["sh", "get-docker.sh"])
+# subprocess.run(["apt-get", "update", "-y"])
+# subprocess.run(["apt-get", "upgrade", "-y"])
+# subprocess.run(["apt-get", "install", "nginx", "-y"])
+# subprocess.run(["curl", "-fsSL", "https://get.docker.com", "-o", "get-docker.sh"])
+# subprocess.run(["sh", "get-docker.sh"])
 
-# ## Checando se os serviços estão com status active
+# # ## Checando se os serviços estão com status active
 
-def servicesToCheck():
-    services = ['docker', 'nginx']
+# def servicesToCheck():
+#     services = ['docker', 'nginx']
 
-    for service in services:
-        process =  subprocess.Popen(["systemctl", "is-active",  service], stdout=subprocess.PIPE)
-        (output, err) = process.communicate()
-        output = output.decode('utf-8')
+#     for service in services:
+#         process =  subprocess.Popen(["systemctl", "is-active",  service], stdout=subprocess.PIPE)
+#         (output, err) = process.communicate()
+#         output = output.decode('utf-8')
         
 
-        if 'inactive' in output:
-            print("Os seguintes serviços estão desativos:", service)
-            # try:
-            #     trying = subprocess.Popen(["systemctl", "start", service], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-            # except:
-            #     print("O serviço:", trying, "falhou a inciar")
-        else:
-            print("Serviço:", service,"ativo")
+#         if 'inactive' in output:
+#             print("Os seguintes serviços estão desativos:", service)
+#             # try:
+#             #     trying = subprocess.Popen(["systemctl", "start", service], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+#             # except:
+#             #     print("O serviço:", trying, "falhou a inciar")
+#         else:
+#             print("Serviço:", service,"ativo")
 
-servicesToCheck()
+# servicesToCheck()
 
 ## Criando e exportando arquivos de virtualhosts do nginx
 
 def touchVirtualHosts():
-    files = ['app1.conf', 'app2.conf', 'app3.conf']
-    locationConfdNginx = "/etc/nginx/conf.d/"
-    
-    subprocess.run(["rm", "-f", locationConfdNginx,"default.conf"])
-    for touching in files:
-            from pathlib import Path
-            Path(locationConfdNginx,touching).touch()
-            subprocess.run(["ls", "-la", locationConfdNginx])
+    fileName = ['app1.conf', 'app2.conf', 'app3.conf']
+    locationConfdNginx = '/home/mrossemo/'
+    confFilesNginx =("server{\nlisten 80;\nserver_name app1.dexter.com.br; location /{\nproxy_pass http://10.215.47.45:8080/;\n}\n}",
+     "server{\nlisten 80;\nserver_name app2.dexter.com.br; location /{\nproxy_pass http://10.215.47.45:8081/;\n}\n}",
+      "server{\nlisten 80;\nserver_name app3.dexter.com.br; location /{\nproxy_pass http://10.215.47.45:8082/;\n}\n}")     
+    for touching, config in zip (fileName, confFilesNginx):
+        with open(locationConfdNginx + touching, 'w+') as file:
+            file.writelines(config)
+
+touchVirtualHosts()
+    # subprocess.run(["rm", "-f", locationConfdNginx,"default.conf"])
