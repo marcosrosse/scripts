7912cab42bfe19f9cfc274efb8c22fb29eddf712
Marcos Rosse <marcosrossem@gmail.com>
2021-03-21 01:47:56 -0300

    aws os doesnt lsb_release pkg

--- inst-minikube.sh
+++ inst-minikube.sh
@@ -15,6 +15,7 @@ then
   else
      curl -sSFL https://get.docker.com | sh
 
+fi
 fi  
     curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64;
     install minikube-linux-amd64 /usr/local/bin/minikube;
