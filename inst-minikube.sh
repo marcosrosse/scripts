f1a9374adebc8dd426c56fa54daec675e80dd359
Marcos Rosse <marcosrossem@gmail.com>
2021-03-21 01:46:36 -0300

    aws os doesnt lsb_release pkg

--- inst-minikube.sh
+++ inst-minikube.sh
@@ -6,22 +6,16 @@ then
     exit
 fi
 
-## Check distros supported by the script in get docker 
-
-MY_DISTRO=$(lsb_release -d|awk '{print $2}')
-O_DISTROS=("Debian" , "Ubuntu" , "Fedora" , "CentOS")
-
-if [ $DISTRO = O_DISTROS ]
+if !  command -v docker -v  &> /dev/null
 then
-    curl -sSFL https://get.docker.com | sh
-else
-    if !  command -v docker -v  &> /dev/null
-      then
-      pacman -Syyu docker;
-      echo "Docker installed"
-    else
+   DISTRO=$( cat /etc/*-release | tr [:upper:] [:lower:] | grep -Poi '(debian|ubuntu|red hat|centos)' | uniq )
+  if [ -z $DISTRO ]
+  then
+     pacman -Syyu docker;
+  else
+     curl -sSFL https://get.docker.com | sh
+
+fi  
     curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64;
     install minikube-linux-amd64 /usr/local/bin/minikube;
     echo "Minikibe has been installed"
-    fi
-fi
