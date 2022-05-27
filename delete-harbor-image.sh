67b1fd7fd703883b34e378f98eaa0fca810c3325
Marcos Rosse <marcosrossem@gmail.com>
2022-05-27 11:17:30 -0300

    Create delete-harbor-image.sh
    script to delete latest images in harbor registry.

---
+++ delete-harbor-image.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+## Simple script to delete latest images in the Harbor before upload a new image.
+## Run this in your CI/CD pipeline
+
+case "${{parameters.environment}}" in
+    "dev")
+        STATUS_CODE=$(curl -u "$(HARBOR_USER)":"$(HARBOR_PASS)" --location --request\
+        GET https://"$(HOST)"/api/v2.0/projects/"$(PROJECT)"/repositories/"$(REPO_NAME)"/artifacts/"${{parameters.tag}}"\
+          -k --write-out "%{http_code}\n" --silent --output /dev/null)
+
+        case "$STATUS_CODE" in
+            404)
+                echo "There's no such tag or image in this repository. Status code: $STATUS_CODE"
+                exit 0
+            ;;
+
+            200)
+                STATUS_CODE=$(curl -u "$(HARBOR_USER)":"$(HARBOR_PASS)" --location --request\
+                  DELETE https://"$(HARBOR_HOST)"/api/v2.0/projects/"$(PROJECT)"/repositories/"$(REPO_NAME)"/artifacts/"${{parameters.tag}}"\
+                  -k --write-out "%{http_code}\n" --silent --output /dev/null)
+
+                if [ "$STATUS_CODE" = 200 ]; then
+                    echo "Image deleted in Harbor. Status code: $STATUS_CODE"
+                    exit 0
+                else
+                    echo "Error when try to delete the image. Status Code "$STATUS_CODE""
+                    exit 1
+                fi
+            ;;
+
+            *)
+            ;;
+
+        esac
+            ;;
+
+            "tst")
+              echo "TST environment. Actually no need to delete an image"
+            ;;
+
+            "uat")
+              echo "UAT environment. Actually no need to delete an image"
+            ;;
+
+            "prd")
+              echo "PRD environment. Actually no need to delete an image"
+            ;;
+
+            *)
+            ;;
+      esac
