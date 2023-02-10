f4a90b962dbfed6d0accb48cda4b9f9f4de350ee
Marcos Rosse <marcosrossem@gmail.com>
2023-02-10 11:09:16 +0000

    add a simple script
    add a simple script to download files from an Openshift POD

---
+++ getElephants.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+## A simple script to copy files from an OpenShift POD.
+## It's recommended to split the file in the POD with the command: split -b 1m <FILE TO BE SPLITED>
+
+FILENAME="files.txt"
+FILESPATH="COPY"
+POD_NAME="jmeter-master-0"
+
+function createAndGetFilename {
+  oc exec $POD_NAME  -- find $FILESPATH -type f -name 'x*' -printf '%f\n' > $FILENAME
+}
+
+function getElephantsFile {
+  CONTENT=$(cat $FILENAME)
+  for line in $CONTENT
+    do
+      echo "-=-=- COPYING $line -=-=-"
+      oc cp $POD_NAME:/$FILESPATH/$line $line
+      echo ""
+      if $? == 0; then
+        echo "$line FILE COPIED SUCCESSFULY"
+        echo ""
+      else
+        echo "ERROR WHILE COPYING THE FILE $line, PLEASE TRY AGAIN LATTER"
+        echo "$line" >> error.txt
+        echo ""
+    done
+}
+createAndGetFilename
+getElephantsFile
