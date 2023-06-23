c1654afe0e76e51e0683811f8227fe7c1f4fe180
Marcos Rosse <marcosrossem@gmail.com>
2023-06-23 13:31:39 +0100

    fix error in the if

--- getElephants.sh
+++ getElephants.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ## A simple script to copy files from an OpenShift POD.
-## It's recommended to split the file in the POD with the command: split -b 1m <FILE TO BE SPLITED>
+## It's recommended to split the file in the POD with the command: split -b 1m <FILE TO BE SPLIT>
 
 FILENAME="files.txt"
 FILESPATH="COPY"
@@ -11,22 +11,22 @@ function createAndGetFilename {
   oc exec $POD_NAME  -- find $FILESPATH -type f -name 'x*' -printf '%f\n' > $FILENAME
 }
 
-function getElephantsFile {
+function getFiles {
   CONTENT=$(cat $FILENAME)
   for line in $CONTENT
     do
       echo "-=-=- COPYING $line -=-=-"
       oc cp $POD_NAME:/$FILESPATH/$line $line
       echo ""
-      if $? == 0; then
-        echo "$line FILE COPIED SUCCESSFULY"
+      if [ $? == 0 ]; then
+        echo "$line FILE COPIED SUCCESSFULLY"
         echo ""
       else
-        echo "ERROR WHILE COPYING THE FILE $line, PLEASE TRY AGAIN LATTER"
+        echo "ERROR WHILE COPYING THE FILE $line, PLEASE TRY AGAIN LATER"
         echo "$line" >> error.txt
         echo ""
       fi
     done
 }
 createAndGetFilename
-getElephantsFile
+getFiles
